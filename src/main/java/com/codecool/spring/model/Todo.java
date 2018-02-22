f61c5923c1046982189c37ddbf82372bff20703c
Soma Szolyák <soma.szolyak@gmail.com>
2018-02-22 23:23:42 +0100

    updates todo

--- src/main/java/com/codecool/spring/model/Todo.java
+++ src/main/java/com/codecool/spring/model/Todo.java
@@ -1,8 +1,55 @@
 package com.codecool.spring.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import java.util.UUID;
 
-@Entity
 public class Todo {
+
+    private String title;
+    private String id;
+    private Status status;
+
+    public Todo() {}
+
+    public Todo(String title, String id, Status status) {
+        this.title = title;
+        this.id = id;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void toggleStatus() {
+        this.status = isComplete() ? Status.ACTIVE : Status.COMPLETE;
+    }
+
+    public boolean isComplete() {
+        return this.status == Status.COMPLETE;
+    }
+
+    public static Todo create(String title) {
+        return new Todo(title, UUID.randomUUID().toString(), Status.ACTIVE);
+    }
+
 }
